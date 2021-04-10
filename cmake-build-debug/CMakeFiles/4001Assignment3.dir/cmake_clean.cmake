1a8073db333977eb74a4dbd9d3b1c80dc4da1375
Haydn Brown <haydnpbrown1@gmail.com>
2021-04-09 21:21:05 -0400

    created empty project

---
+++ cmake-build-debug/CMakeFiles/4001Assignment3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "4001Assignment3"
+  "4001Assignment3.pdb"
+  "CMakeFiles/4001Assignment3.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/4001Assignment3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
