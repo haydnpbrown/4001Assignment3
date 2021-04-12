60b8ce8e31d3e4f3fae54de8764871c97a88d2e4
haydnpbrown <haydnpbrown1@gmail.com>
2021-04-11 20:45:21 -0400

    iteration 1 is complete

--- main.c
+++ main.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 int main() {
-    printf("Hello, penis\n");
-    printf("Hello, its chase\n");
+
+    pid_t pid;
+
+    printf("fork program starting \n");
+
+    pid = fork();
+
+    if (pid == -1){
+        printf("error forking");
+    } else if (pid == 0) {
+        printf("I am process 1 (child) \n");
+        while(1){
+        }
+    } else {
+        printf("I am process 2 (parent) \n");
+        while(1){
+
+        }
+    }
+
     return 0;
 }
